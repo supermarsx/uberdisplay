7e3437b4a8bf935cd9a1dafa4c9f80e1295b7718
supermarsx <github_618a@imariana.com>
2026-01-18 14:41:57 +0000

    Add ActionMenuActivity for action menu UI
    Introduces ActionMenuActivity as a new AppCompatActivity that sets its content view to activity_action_menu layout. This sets up the structure for the action menu screen in the app.

---
+++ android/app/src/main/java/com/supermarsx/uberdisplay/ui/ActionMenuActivity.kt
@@ -0,0 +1,12 @@
+package com.supermarsx.uberdisplay.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.supermarsx.uberdisplay.R
+
+class ActionMenuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_action_menu)
+    }
+}
